f69f929c01961b4da3b9d9f80831c07209265b3b
Mamadou Lamine Ba <lamine.ba@thinktech.sn>
2018-03-01 11:25:05 +0000

    improving the project.

--- WebContent/modules/payment/scripts/module.groovy
+++ WebContent/modules/payment/scripts/module.groovy
@@ -3,13 +3,17 @@ import groovy.sql.Sql
 class ModuleAction extends ActionSupport {
 
     def payBill(){
-      def bill = parse(request) 
-      println bill
-      def modules = moduleManager.modules
-      modules.each{
-         println it.name
-      }
-	  json([status: 1])
+       response.addHeader("Access-Control-Allow-Origin", "*");
+       response.addHeader("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+       if(request.method == "POST") { 
+	      def bill = parse(request) 
+	      println bill
+	      def modules = moduleManager.modules
+	      modules.each{
+	         println it.name
+	      }
+		  json([status: 1])
+	   }
    }
    
    def getConnection()  {

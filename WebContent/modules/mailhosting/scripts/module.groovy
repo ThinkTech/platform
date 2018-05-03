bb6ddd63e0d9b20bc4b842890d8be5f030e39085
Thinktech <dev@thinktech.sn>
2018-05-03 16:04:33 +0000

    improving the project.

--- WebContent/modules/mailhosting/scripts/module.groovy
+++ WebContent/modules/mailhosting/scripts/module.groovy
@@ -3,6 +3,15 @@ import groovy.sql.Sql
 class ModuleAction extends ActionSupport {
     
 	def subscribe(subscription) {
+	   def connection = getConnection()
+	   def ticket = new Expando()
+	   ticket.subject = "configuration business email"
+	   ticket.service = subscription.service
+	   ticket.message = "Configuration de votre business email - plan "+subscription.plan
+	   def user = subscription.user
+	   def params = [ticket.subject,ticket.service,ticket.message,user.id,user.structure_id]
+       connection.executeInsert 'insert into tickets(subject,service,message,user_id,structure_id) values (?, ?, ?,?,?)', params
+	   connection.close()
     }
    
 	def getConnection() {

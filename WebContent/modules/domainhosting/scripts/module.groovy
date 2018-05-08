b9390dcfcbdba01f6018be32a067034ce1e47c20
Thinktech <dev@thinktech.sn>
2018-05-08 15:03:04 +0000

    improving the project.

--- WebContent/modules/domainhosting/scripts/module.groovy
+++ WebContent/modules/domainhosting/scripts/module.groovy
@@ -15,17 +15,15 @@ class Service extends ActionSupport {
 	def verify(){
 	    response.addHeader("Access-Control-Allow-Origin", "*");
         response.addHeader("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");   
-	    if(request.method == "GET") { 
-	        def domain = getParameter("domain")
-		    if(domain){
-		        def url = "https://api.duoservers.com/?auth_username=store203583&auth_password=apipassword&section=domains&command=check&name=${domain}&tlds[0]=com&tlds[1]=net&tlds[2]=org&tlds[3]=biz&tlds[4]=info&tlds[5]=tv&tlds[6]=press&tlds[7]=news&tlds[8]=tech&return_type=json"
-		    	def get = new HttpGet(url)
-				def client = HttpClientBuilder.create().build()
-				def response = client.execute(get)
-				def responseBody = EntityUtils.toString(response.getEntity())
-				write(responseBody)
-		    }
-	    }
+	    def domain = getParameter("domain")
+	    if(domain){
+		    def url = "https://api.duoservers.com/?auth_username=store203583&auth_password=apipassword&section=domains&command=check&name=${domain}&tlds[0]=com&tlds[1]=net&tlds[2]=org&tlds[3]=biz&tlds[4]=info&tlds[5]=tv&tlds[6]=press&tlds[7]=news&tlds[8]=tech&return_type=json"
+		    def get = new HttpGet(url)
+			def client = HttpClientBuilder.create().build()
+			def response = client.execute(get)
+			def responseBody = EntityUtils.toString(response.getEntity())
+			write(responseBody)
+		}
 	}
 
 }

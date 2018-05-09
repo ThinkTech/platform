d22fb31eb06c6581584aaaa686fe3e39d9e68087
Thinktech <dev@thinktech.sn>
2018-05-09 20:40:05 +0000

    improving the project.

--- WebContent/modules/domainhosting/scripts/module.groovy
+++ WebContent/modules/domainhosting/scripts/module.groovy
@@ -14,7 +14,8 @@ class Service extends ActionSupport {
 	
 	def verify(){
 	    response.addHeader("Access-Control-Allow-Origin", "*");
-        response.addHeader("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");   
+        response.addHeader("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+        response.addHeader("Cache-control", "private, max-age=7200")   
 	    def domain = getParameter("domain")
 	    if(domain){
 		    def url = "https://api.duoservers.com/?auth_username=store203583&auth_password=apipassword&section=domains&command=check&name=${domain}&tlds[0]=com&tlds[1]=net&tlds[2]=org&tlds[3]=biz&tlds[4]=info&tlds[5]=tv&tlds[6]=press&tlds[7]=news&tlds[8]=tech&return_type=json"
@@ -22,7 +23,6 @@ class Service extends ActionSupport {
 			def client = HttpClientBuilder.create().build()
 			def response = client.execute(get)
 			def body = EntityUtils.toString(response.getEntity())
-			response.setHeader("Cache-control", "private, max-age=7200")
 			write(body)
 		}
 	}

e918a6e5bb61ecde092b7e05feafe89b4b7182ff
ThinkTech <dev@thinktech.sn>
2018-06-19 20:56:58 +0000

    improving the project.

--- WebContent/modules/mailhosting/scripts/module.groovy
+++ WebContent/modules/mailhosting/scripts/module.groovy
@@ -64,7 +64,7 @@ class Service extends ActionSupport {
 	def pay(bill){
 		def order = connection.firstRow("select * from  domains  where id = ?", [bill.product_id])
         sendMail(user.name,user.email,"Configuration business email pour le domaine ${order.name} en cours",getConfirmationTemplate(order))
-        sendMail("ThinkTech Support","support@thinktech.sn","Configuration business email pour le domaine ${order.domain} en cours",getSupportTemplate(order))
+        sendMail("ThinkTech Support","support@thinktech.sn","Configuration business email pour le domaine ${order.name} en cours",getSupportTemplate(order))
 	}
 	
 	def getOrderTemplate(order) {
@@ -164,10 +164,11 @@ class Service extends ActionSupport {
 		     h5(style : "font-size: 90%;color: rgb(0, 0, 0);margin-bottom: 0px") {
 		         span("Domaine : $order.name")
 		     }
-		     p("le paiement de la facture a &eacute;t&eacute; bien effectu&eacute; et le business email est maintenant en cours de configuration.")
+		     p("le paiement de la facture a &eacute;t&eacute; bien effectu&eacute; et le business email est maintenant en cours de configuration.
+		     Cliquer sur le bouton Configurer en bas pour d&eacute;marrer la r&eacute;solution du ticket correspondant.")
 		    }
 		    div(style : "text-align:center;margin-top:30px;margin-bottom:10px") {
-			    a(href : "$url/dashboard/domains",style : "font-size:150%;width:180px;margin:auto;text-decoration:none;background: #05d2ff;display:block;padding:10px;border-radius:2px;border:1px solid #eee;color:#fff;") {
+			    a(href : "$url/dashboard/support",style : "font-size:150%;width:180px;margin:auto;text-decoration:none;background: #05d2ff;display:block;padding:10px;border-radius:2px;border:1px solid #eee;color:#fff;") {
 			        span("Configurer")
 			    }
 			}

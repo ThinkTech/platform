143ec3dc0515ebed27a49c36805c7103967b1a0e
ThinkTech <dev@thinktech.sn>
2018-07-05 19:52:01 +0000

    improving the project.

--- WebContent/modules/mailhosting/scripts/module.groovy
+++ WebContent/modules/mailhosting/scripts/module.groovy
@@ -37,7 +37,7 @@ class Service extends ActionSupport {
          service = "mailhosting"
          message = "<p>Configuration email pour le domaine "+order.domain+" suivant le plan "+order.plan+"</p>"
        }
-       ticket.message += "<p>Super Administrateur : "+order.email+"@"+order.domain+"</p>"
+       ticket.message += "<p>Super Administrateur Email : "+order.email+"@"+order.domain+"</p>"
        params = [ticket.subject,ticket.service,ticket.message,user.id,user.structure_id,product_id,true]
        connection.executeInsert 'insert into tickets(subject,service,message,user_id,structure_id,product_id,autoClose) values (?,?,?,?,?,?,?)', params
        def bill = createBill(order)

67e90b2cfb187853a6edd8ce7e4922bbe10521b5
ThinkTech <dev@thinktech.sn>
2018-07-19 18:59:20 +0000

    improving the project.

--- WebContent/modules/mailhosting/scripts/module.groovy
+++ WebContent/modules/mailhosting/scripts/module.groovy
@@ -129,7 +129,7 @@ class Service extends ActionSupport {
 		     h5(style : "font-size: 90%;color: rgb(0, 0, 0);margin-bottom: 0px") {
 		         span("Domaine : $order.name")
 		     }
-		     p("Votre business email est en cours de configuration et le ticket est en cours de r&eacute;solution par notre &eacute;quipe technique. Nous vous contacterons sou peu pour la validation des informations fournies.")
+		     p("Votre business email est en cours de configuration et le ticket est en cours de r&eacute;solution par notre &eacute;quipe technique. Nous vous contacterons sou peu pour la validation des informations fournies ou vous pouvez juste ajouter un commentaire au ticket pour confirmation.")
 		    }
 		    div(style : "text-align:center;margin-top:30px;margin-bottom:10px") {
 			    a(href : "$url/dashboard/support",style : "font-size:130%;width:140px;margin:auto;text-decoration:none;background: #05d2ff;display:block;padding:10px;border-radius:2px;border:1px solid #eee;color:#fff;") {

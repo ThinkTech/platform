30b29d70b62ae4b6aa7288f816e5dd94a29422b2
ThinkTech <dev@thinktech.sn>
2018-07-21 14:12:39 +0000

    improving the project.

--- WebContent/modules/main/scripts/module.groovy
+++ WebContent/modules/main/scripts/module.groovy
@@ -27,7 +27,7 @@ class Dispatcher extends ActionSupport {
                       structure_id = result[0][0]
                  	}	        
 		            params = [user.name,user.email,user.password,"administrateur","manager",user.telephone,true,user.structure_id]
-		            result = connection.executeInsert 'insert into users(name,email,password,role,profession,telephone,owner,structure_id) values (?,?,sha(?),?,?,?,?)', params
+		            result = connection.executeInsert 'insert into users(name,email,password,role,profession,telephone,owner,structure_id) values (?,?,sha(?),?,?,?,?,?)', params
 		            user.id = result[0][0]
 		            def alphabet = (('A'..'N')+('P'..'Z')+('a'..'k')+('m'..'z')+('2'..'9')).join()  
 		 	        def n = 30 

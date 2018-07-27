40c49cbe7f7f7c193fd40e8faea19c4d1f50115c
ThinkTech <dev@thinktech.sn>
2018-07-27 09:16:31 +0000

    improving the project.

--- WebContent/modules/webdev/scripts/module.groovy
+++ WebContent/modules/webdev/scripts/module.groovy
@@ -130,7 +130,7 @@ class Service extends ActionSupport {
 		    img(src : "https://www.thinktech.sn/images/logo.png", style : "display:block;margin : 0 auto")
 		    div(style : "margin-top:10px;padding-bottom:2%;padding-top:2%;text-align:center;background:#05d2ff") {
 		      h4(style : "font-size: 120%;color: #fff;margin: 3px") {
-		        span("votre projet est en attente de traitement")
+		        span("Projet en attente de traitement")
 		      }
 		       p(style : "font-size:100%;color:#fff"){
 			        span("cliquer sur le bouton en bas pour effectuer le paiement de la caution")
@@ -206,7 +206,7 @@ class Service extends ActionSupport {
 		    img(src : "https://www.thinktech.sn/images/logo.png", style : "display:block;margin : 0 auto")
 		    div(style : "margin-top:10px;padding-bottom:2%;padding-top:2%;text-align:center;background:#05d2ff") {
 		      h4(style : "font-size: 120%;color: #fff;margin: 3px") {
-		        span("votre projet est en attente de traitement")
+		        span("Projet en attente de traitement")
 		      }
 		    }
 		    div(style : "width:90%;margin:auto;margin-top : 30px;margin-bottom:30px") {
@@ -244,7 +244,7 @@ class Service extends ActionSupport {
 		    img(src : "https://www.thinktech.sn/images/logo.png", style : "display:block;margin : 0 auto")
 		    div(style : "margin-top:10px;padding-bottom:2%;padding-top:2%;text-align:center;background:#05d2ff") {
 		      h4(style : "font-size: 120%;color: #fff;margin: 3px") {
-		        span("votre projet est en cours de traitement")
+		        span("Projet en cours de traitement")
 		      }
 		    }
 		    div(style : "width:90%;margin:auto;margin-top : 30px;margin-bottom:30px") {

0b5432d50204dcb78ce1513cfd2c2d9a5a78a664
ThinkTech <dev@thinktech.sn>
2018-07-27 10:58:41 +0000

    improving the project.

--- WebContent/modules/domainhosting/scripts/module.groovy
+++ WebContent/modules/domainhosting/scripts/module.groovy
@@ -1,5 +1,4 @@
 import org.apache.http.HttpResponse
-import org.apache.http.client.ClientProtocolException
 import org.apache.http.client.HttpClient
 import org.apache.http.client.methods.HttpGet
 import org.apache.http.impl.client.HttpClientBuilder

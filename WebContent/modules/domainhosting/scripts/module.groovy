156c93710bb9e0e85a53b5fbe2edcbbec410ab08
ThinkTech <dev@thinktech.sn>
2018-07-30 09:08:34 +0000

    improving the project.

--- WebContent/modules/domainhosting/scripts/module.groovy
+++ WebContent/modules/domainhosting/scripts/module.groovy
@@ -51,7 +51,7 @@ class Service extends ActionSupport {
 		    img(src : "https://www.thinktech.sn/images/logo.png", style : "display:block;margin : 0 auto")
 		    div(style : "margin-top:10px;padding-bottom:2%;padding-top:2%;text-align:center;background:#3abfdd") {
 		      h4(style : "font-size: 120%;color: #fff;margin: 3px") {
-		        span("Enregistrement du domaine web en attente")
+		        span("Enregistrement domaine web en attente")
 		      }
 		       p(style : "font-size:100%;color:#fff"){
 			        span("cliquer sur le bouton en bas pour effectuer le paiement")
@@ -101,7 +101,7 @@ class Service extends ActionSupport {
 		    img(src : "https://www.thinktech.sn/images/logo.png", style : "display:block;margin : 0 auto")
 		    div(style : "margin-top:10px;padding-bottom:2%;padding-top:2%;text-align:center;background:#3abfdd") {
 		      h4(style : "font-size: 120%;color: #fff;margin: 3px") {
-		        span("Enregistrement du domaine web en attente")
+		        span("Enregistrement domaine web en attente")
 		      }
 		    }
 		    div(style : "width:90%;margin:auto;margin-top : 20px;margin-bottom:30px") {
@@ -149,7 +149,7 @@ class Service extends ActionSupport {
 		    img(src : "https://www.thinktech.sn/images/logo.png", style : "display:block;margin : 0 auto")
 		    div(style : "margin-top:10px;padding-bottom:2%;padding-top:2%;text-align:center;background:#3abfdd") {
 		      h4(style : "font-size: 120%;color: #fff;margin: 3px") {
-		        span("Enregistrement du domaine web en cours")
+		        span("Enregistrement domaine web en cours")
 		      }
 		    }
 		    div(style : "width:90%;margin:auto;margin-top : 20px;margin-bottom:30px") {
@@ -193,7 +193,7 @@ class Service extends ActionSupport {
 		    img(src : "https://www.thinktech.sn/images/logo.png", style : "display:block;margin : 0 auto")
 		    div(style : "margin-top:10px;padding-bottom:2%;padding-top:2%;text-align:center;background:#3abfdd") {
 		      h4(style : "font-size: 120%;color: #fff;margin: 3px") {
-		        span("Enregistrement du domaine web en cours")
+		        span("Enregistrement domaine web en cours")
 		      }
 		    }
 		    div(style : "width:90%;margin:auto;margin-top : 20px;margin-bottom:30px") {

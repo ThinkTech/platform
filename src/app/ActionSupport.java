d55930b5781970b3ac4d2d16433dc1cc28bd3b29
thinktech <dev@thinktech.sn>
2018-09-27 23:03:19 +0000

    improving the project.

---
+++ src/app/ActionSupport.java
@@ -0,0 +1,18 @@
+package app;
+
+import java.util.Map;
+import groovy.text.markup.MarkupTemplateEngine;
+
+@SuppressWarnings("serial")
+public class ActionSupport extends org.metamorphosis.core.ActionSupport {
+	
+	public void sendSupportMail(String object,String content){
+		sendMail("ThinkTech Support","support@thinktech.sn",object,content);
+	}
+	
+	@SuppressWarnings("rawtypes")
+	public String parseTemplate(String template, Map map) throws Exception {
+		return new MarkupTemplateEngine().createTemplate(readFile("templates/"+template+".groovy")).make(map).toString();
+	}
+
+}

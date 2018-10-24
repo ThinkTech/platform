e2b5cabc2893012014fb14621974d34d8e554de7
thinktech <dev@thinktech.sn>
2018-10-24 15:53:02 +0000

    improving the project.

--- src/app/FileManager.java
+++ src/app/FileManager.java
@@ -8,13 +8,12 @@ import com.dropbox.core.v2.files.WriteMode;
 
 public class FileManager {
 	
-	private static final String ACCESS_TOKEN = "pQXsY7k7n6AAAAAAAAAAPWBEAjxZy5Xcgtsd8kyz9VsKOIrEzorICsW29BV4bspM";
-    private final DbxClientV2 client;
+	private final DbxClientV2 client;
     private final String folder;
     
     public FileManager(String folder) {
     	this.folder = folder;
-    	client = new DbxClientV2(new DbxRequestConfig("dropbox/thinktech-app"), ACCESS_TOKEN);
+    	client = new DbxClientV2(new DbxRequestConfig("dropbox/thinktech-app"),System.getenv("dropbox.key"));
     }
     
     public void upload(String name,InputStream in) throws Exception {
